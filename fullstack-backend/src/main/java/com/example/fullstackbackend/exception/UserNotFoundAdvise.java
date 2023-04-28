eb7d69d201e0970e1283dcce07130e687fa95a1a
thinh du <tonyzzz1997@onyzzz1997gmail.z1997gmailcom>
2023-04-28 14:05:13 +0700

    1st

---
+++ fullstack-backend/src/main/java/com/example/fullstackbackend/exception/UserNotFoundAdvise.java
@@ -0,0 +1,22 @@
+package com.example.fullstackbackend.exception;
+
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserNotFoundAdvise {
+    @ResponseBody
+    @ExceptionHandler(UserNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+        public Map<String,String> exceptionHandler(UserNotFoundException exception){
+            Map<String,String> errorMap = new HashMap<>();
+            errorMap.put("errorMessage",exception.getMessage());
+            return errorMap;
+        }
+}
---
+++ fullstack-backend/src/main/java/com/example/fullstackbackend/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.fullstackbackend.repository;
+
+import com.example.fullstackbackend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
